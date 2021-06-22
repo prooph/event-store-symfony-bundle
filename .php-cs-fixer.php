8a718d1b7eabb569abfc88112006739ea36fe300
unixslayer <piotr.zajac@unixslayer.pl>
2021-06-22 15:37:51 +0200

    github actions - tests - static analyse

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,8 +3,4 @@
 $config = new Prooph\CS\Config\Prooph();
 $config->getFinder()->in(__DIR__)->exclude('vendor')->exclude('test/Command/Fixture/var');
 
-$cacheDir = getenv('TRAVIS') ? getenv('HOME') . '/.php-cs-fixer' : __DIR__;
-
-$config->setCacheFile($cacheDir . '/.php_cs.cache');
-
 return $config;
